b9b1a4f697771a4e98bdbfeeddaa90220bd3fe82
istkai <istkaai@gmail.com>
2019-12-29 16:52:23 -0300

    tabbed and dwm patches

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SF Mono:pixelsize=10:antialias=true:autohint=true";
+static char *font = "SF Mono:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
