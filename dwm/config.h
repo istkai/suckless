cee4bf4a6eb1ab0fd4125e99a8f58fc0928a79b6
istkai <istkaai@gmail.com>
2019-12-29 22:24:30 -0300

    alpha

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      =  8;        /* snap pixel */
 static const unsigned int gappx     =  6;
 static const int showbar            =  1;        /* 0 means no bar */
 static const int topbar             =  1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"SF Mono:size=8" };
+static const char *fonts[]          = {"SF Pro Display:size=8" };
 static const char dmenufont[]       =  "SF Mono:size=8";
 static const char col_gray1[]       =  "#222222";
 static const char col_gray2[]       =  "#444444";
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { col_fg, col_bg,      col_magenta },
 	[SchemeSel]  = { col_bg, col_purple,  col_purple  },
 };
-static const unsigned int baralpha = 0xbfU;
+static const unsigned int baralpha = 0xbf;
 static const unsigned int borderalpha = OPAQUE;
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
