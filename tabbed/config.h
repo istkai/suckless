92588fab13bd54966174c7bc258f37e4173b43e2
istkai <istkaai@gmail.com>
2019-12-29 23:12:31 -0300

    tabbed improvements

--- tabbed/config.h
+++ tabbed/config.h
@@ -52,7 +52,7 @@ static Key keys[] = { \
 	{ MODKEY,                       XK_9,      move,           { .i = 8 } },
 	{ MODKEY,                       XK_0,      move,           { .i = 9 } },
 
-	{ MODKEY,                       XK_q,      killclient,     { 0 } },
+	{ MODKEY,                       XK_w,      killclient,     { 0 } },
 
 	{ 0,                            XK_F11,    fullscreen,     { 0 } },
 };
