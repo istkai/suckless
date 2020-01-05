4741989ccdd7ac8793070c543d1e3e4668e0f770
istkai <istkaai@gmail.com>
2020-01-05 00:57:19 -0300

    allgemein

--- dwm/config.h
+++ dwm/config.h
@@ -2,10 +2,10 @@
 
 /* appearance */
 static const unsigned int borderpx  =  0;        /* border pixel of windows */
-static const unsigned int snap      =  0;        /* snap pixel */
+static const unsigned int snap      =  1;        /* snap pixel */
 static const unsigned int gappx     =  10;
 static const int showbar            =  1;        /* 0 means no bar */
-static const int topbar             =  1;        /* 0 means bottom bar */
+static const int topbar             =  0;        /* 0 means bottom bar */
 static const char *fonts[]          = {"SF Pro Text:size=8" };
 static const char dmenufont[]       =  "SF Pro Text:size=8";
 static const char col_gray1[]       =  "#222222";
